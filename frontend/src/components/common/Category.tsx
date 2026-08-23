91810d394c5739b330296798c4cf7ee2c0397e46
pjcb-a <johnpauljacob15@gmail.com>
2026-08-23 17:52:52 +0800

    feat: category filter

--- frontend/src/components/common/Category.tsx
+++ frontend/src/components/common/Category.tsx
@@ -1,15 +1,33 @@
 import { projects } from "../../data/projects";
+import { useState } from "react";
+import "../../styles/common/category.css"
 
 export default function Category() {
+    const [selectedCategory, setSelectedCategory] = useState("All");
+
+    const categories = [
+        "All",
+        ...new Set(projects.map((project) => project.category)),
+    ];
+
+    // const filteredProjects = 
+    //         selectedCategory === "All"
+    //         ? projects
+    //         : projects.filter(
+    //             (project) => project.category === selectedCategory
+    //         );
+
     return (
         <div className="category-container">
-        <ul className="category">
-            {projects.map((item) => (
-                <li key={item.categoryLabel}>
-                    {item.category}
-                </li>
-            ))}
-        </ul>
+                {categories.map((category) => (
+                    <button
+                    key={category}
+                    onClick={() => setSelectedCategory(category)}
+                    className={selectedCategory === category ? "active" : ""}
+                    >
+                        {category}
+                    </button>
+                ))}
         </div>
     );
 }
