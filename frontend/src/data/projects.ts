805a0221c2dd1b46cfc08670f15b8e8b2a9c4d64
pjcb-a <johnpauljacob15@gmail.com>
2026-09-13 22:47:03 +0800

    feat: added some data for the projects page

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -89,27 +89,160 @@ export const projects: Project[] = [
 
   {
     id: 4,
-    slug: "memorandum",
+    slug: "forest",
 
-    title: "Memorandum",
+    title: "Forest",
     category: "poster",
     categoryLabel: "POSTERS",
 
     year: 2026,
-    subtitle: "Organization Social Media Post",
+    subtitle: "Personal Project",
 
     description:
-      "A poster for information.",
+      "A poster for inspiration.",
 
     role: [
       "Graphic Designer",
     ],
 
     tools: [
-      "Photoshop",
+      "Photoshop", 
     ],
 
-    image: "/projects/memorandum/cover.png",
+    image: "/projects/forest/cover.png",
+
+    featured: false,
+  },
+
+  {
+    id: 5,
+    slug: "subtrack",
+
+    title: "SubTrack",
+    category: "development",
+    categoryLabel: "DEVELOPMENT",
+
+    year: 2026,
+    subtitle: "Trends in Application Development Project",
+
+    description:
+      "A web-based subscription tracker for notifying users of payments before the date of renewals and a centralized expense dashboard of subscriptions.",
+
+    role: [
+      "Frontend Developer",
+      "UI / UX Designer",
+    ],
+
+    tools: [
+      "Figma",
+      "Vue",
+    ],
+
+    image: "/projects/subtrack/cover.png",
+
+    featured: false,
+  },
+
+  {
+    id: 6,
+    slug: "ADNU-L Scoring Database System",
+    title: "ADNU-L SDBS",
+    category: "development",
+    categoryLabel: "DEVELOPMENT",
+    year: 2025,
+    subtitle: "Advanced Database Systems Project",
+    description: "A comprehensive web-based scoring and management system for ADNU League events and competitions.",
+    role: [
+      "Frontend Developer",
+      "UI/UX Designer"
+    ],
+    tools: [
+      "Figma",
+      "Vue"
+    ],
+    image: "/projects/adnu-lsdbs/cover.png",
+    featured: false,
+  },
+
+   {
+    id: 7,
+    slug: "nature-1",
+
+    title: "Sentimental Nature",
+    category: "poster",
+    categoryLabel: "POSTERS",
+
+    year: 2026,
+    subtitle: "Personal Project",
+
+    description:
+      "A poster showcasing sentimental quotes while admiring nature in its raw form.",
+
+    role: [
+      "Graphic Designer",
+    ],
+
+    tools: [
+      "Photoshop", "Canva"
+    ],
+
+    image: "/projects/nature-1/cover.png",
+
+    featured: false,
+  },
+
+   {
+    id: 8,
+    slug: "chinese new year",
+
+    title: "Chinese New Year",
+    category: "creative-media",
+    categoryLabel: "CREATIVE MEDIA",
+
+    year: 2026,
+    subtitle: "Graphic Design / Event Identity",
+
+    description:
+      "A social media poster for ADNU Lideratos celebrating chinese new year.",
+
+    role: [
+      "Graphic Designer"
+    ],
+
+    tools: [
+      "Canva"
+    ],
+
+    image: "/projects/cny/cover.png",
+
+    featured: false,
+  },
+
+  {
+    id: 9,
+    slug: "memorandum",
+
+    title: "MEMORANDUM",
+    category: "creative-media",
+    categoryLabel: "CREATIVE MEDIA",
+
+    year: 2025,
+    subtitle: "Graphic Design / Public Media",
+
+    description:
+      "A simple design showcasing updates regarding the organizations plan and transparency reports.",
+
+    role: [
+      "Graphic Designer",
+      "Creatives",
+    ],
+
+    tools: [
+      "Figma",
+      "Canva"
+    ],
+
+    image: "/projects/memo/cover.png",
 
     featured: false,
   },
